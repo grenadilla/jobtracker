210d20e15d9e598d6bc123d7976ecf6c61d07b8c
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2021-04-08 22:13:17 -0500

    Add basic query all routes w/ pagination

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,20 @@
 from app import db
+from sqlalchemy import select
 
 def fetch_user(user_id):
     conn = db.connect()
     query_results = conn.execute(f'SELECT * FROM User WHERE id = {user_id};').fetchall()
     conn.close()
     return [dict(zip(["id", "username", "password", "name", "grade", "GPA"], result)) for result in query_results]
+
+def get_all(table, attributes, page, per_page):
+    conn = db.connect()
+    query_results = conn.execute(f'SELECT * FROM {table} LIMIT {per_page} OFFSET {(page - 1) * per_page}')
+    conn.close()
+    return [dict(zip(attributes, result)) for result in query_results]
+
+def all_companies(page, per_page):
+    return get_all('Company', ["id", "name", "website", "description"], page, per_page)
+
+def all_postings(page, per_page):
+    return get_all('Posting', ["id", "title", "description", "location", "link", "due_date", "posted_by"], page, per_page)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -10,3 +10,23 @@ def homepage():
 def get_user(user_id):
     query_result = database.fetch_user(user_id)
     return jsonify(query_result[0])
+
+def get_all(query_func):
+    try:
+        page = int(request.args.get('page'))
+    except:
+        page = 1
+    try:
+        per_page = int(request.args.get('per_page'))
+    except:
+        per_page = 25
+    query_result = query_func(page, per_page)
+    return jsonify(query_result)
+
+@app.route('/company/all', methods=["GET"])
+def get_companies():
+    return get_all(database.all_companies)
+
+@app.route('/posting/all', methods=["GET"])
+def get_postingss():
+    return get_all(database.all_postings)
