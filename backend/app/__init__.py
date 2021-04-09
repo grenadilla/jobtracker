2b6e5401190b55c1856cd44203abbc757b33bc25
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2021-04-09 18:57:30 -0500

    Basic CORS whitelist

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,27 +2,27 @@ import os
 import sqlalchemy
 from yaml import load, Loader
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-def init_connect_engine():
-    if os.environ.get('GAE_ENV') != 'standard':
-        variables = load(open("app.yaml"), Loader=Loader)
-        env_variables = variables['env_variables']
-        for var in env_variables:
-            os.environ[var] = env_variables[var]
+if os.environ.get('GAE_ENV') != 'standard':
+    variables = load(open("app.yaml"), Loader=Loader)
+    env_variables = variables['env_variables']
+    for var in env_variables:
+        os.environ[var] = env_variables[var]
 
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL(
-            drivername="mysql+pymysql",
-            username=os.environ.get("MYSQL_USER"),
-            password=os.environ.get("MYSQL_PASSWORD"),
-            database=os.environ.get("MYSQL_DB"),
-            host=os.environ.get("MYSQL_HOST")
-        )
+db = sqlalchemy.create_engine(
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username=os.environ.get("MYSQL_USER"),
+        password=os.environ.get("MYSQL_PASSWORD"),
+        database=os.environ.get("MYSQL_DB"),
+        host=os.environ.get("MYSQL_HOST")
     )
-    return pool
+)
 
-db = init_connect_engine()
+# TODO edit CORS whitelist in yaml when uploading to gcp
+CORS(app, origins=os.environ.get("FRONTEND"))
 
 from app import routes
